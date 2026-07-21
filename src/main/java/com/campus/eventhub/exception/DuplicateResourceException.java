7be741128b3ca24c20774104a87656693d0d1139
Le Pham <phamhongle412004@gmail.com>
2026-07-21 15:15:03 +0700

    Định nghĩa các ngoại lệ

---
+++ src/main/java/com/campus/eventhub/exception/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package com.campus.eventhub.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
